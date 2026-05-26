7c11851238ade77c7b6391da4c6e025543ac6575
Yash2930 <Yashv1062@gmail.com>
2026-05-26 21:44:24 +0530

    Implemented BCrypt password encryption

--- src/main/java/com/hms/hospital_management_system/config/SecurityConfig.java
+++ src/main/java/com/hms/hospital_management_system/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 public class SecurityConfig {
 
@@ -28,5 +31,10 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- src/main/java/com/hms/hospital_management_system/service/impl/AuthServiceImpl.java
+++ src/main/java/com/hms/hospital_management_system/service/impl/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import com.hms.hospital_management_system.entity.User;
 import com.hms.hospital_management_system.repository.UserRepository;
 import com.hms.hospital_management_system.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,7 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public String register(RegisterRequest request) {
@@ -25,7 +27,7 @@ public class AuthServiceImpl implements AuthService {
         User user = User.builder()
                 .fullName(request.getFullName())
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .role(request.getRole())
                 .createdAt(LocalDateTime.now())
                 .build();
